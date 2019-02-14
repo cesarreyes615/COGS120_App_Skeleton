ede5f8cb1b4375fbe0374d6bb42d556d4a3564b5
Cesar Reyes <car047@ucsd.edu>
2019-02-13 20:42:04 -0800

    Updates

---
+++ js/login.js
@@ -0,0 +1,13 @@
+var infoArray = [];
+
+$('#login').submit(function(event) {
+    var email = $('#email').val();
+    var password = $('#password').val();
+
+    
+    infoArray[0] = email;
+    console.log(inforArray[0]);
+
+});
+
+
